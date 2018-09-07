02e599ffb9b01bd1c57e3bb06894ebd67db15d7a
rchoudhury <rchoudhury@ZALONILABS.COM>
2018-09-07 16:15:10 +0530

    Initial app setup and dependency

---
+++ no-ones-book/src/main/java/com/rj/noonesbook/NoOnesBookApplication.java
@@ -0,0 +1,12 @@
+package com.rj.noonesbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoOnesBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoOnesBookApplication.class, args);
+	}
+}
