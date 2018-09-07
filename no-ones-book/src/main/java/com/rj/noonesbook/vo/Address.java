2270290d89d7ce7c23c9bf014f3f35b1bdf51e90
jchakraborty <jchakraborty@zaloni.com>
2018-09-07 17:42:50 +0530

    vo layer

---
+++ no-ones-book/src/main/java/com/rj/noonesbook/vo/Address.java
@@ -0,0 +1,49 @@
+package com.rj.noonesbook.vo;
+
+public class Address {
+    String houseNum;
+    String street;
+    String pin;
+    String city;
+    String state;
+
+    public String getHouseNum() {
+        return houseNum;
+    }
+
+    public void setHouseNum(String houseNum) {
+        this.houseNum = houseNum;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
--- no-ones-book/src/main/java/com/rj/noonesbook/vo/Item.java
+++ no-ones-book/src/main/java/com/rj/noonesbook/vo/Item.java
@@ -1,5 +1,40 @@
 package com.rj.noonesbook.vo;
 
 public class Item {
+    String name;
+    Person owner;
+    float rate;
+    String heading;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
 }
