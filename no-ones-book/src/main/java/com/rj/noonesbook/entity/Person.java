86c8919708fa8f8e6bf76f8c9731fd8312a3fd31
rchoudhury <rchoudhury@ZALONILABS.COM>
2018-09-07 20:14:33 +0530

    Minor fix

--- no-ones-book/src/main/java/com/rj/noonesbook/entity/Person.java
+++ no-ones-book/src/main/java/com/rj/noonesbook/entity/Person.java
@@ -1,51 +1,49 @@
 package com.rj.noonesbook.entity;
 
-
-
 import javax.persistence.*;
 
 @Entity(name = "PERSON")
 public class Person {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    int personId;
-    @Column(name = "name")
-    String name;
-    @OneToOne(mappedBy = "person")
-    Item item;
-    @OneToOne
-    @JoinColumn(name = "address")
-    Address address;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int personId;
+	@Column(name = "name")
+	String name;
+	@OneToMany(mappedBy = "person")
+	ItemDTO itemDTO;
+	@OneToMany
+	@JoinColumn(name = "address")
+	Address address;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public Address getAddress() {
-        return address;
-    }
+	public Address getAddress() {
+		return address;
+	}
 
-    public void setAddress(Address address) {
-        this.address = address;
-    }
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 
-    public int getId() {
-        return personId;
-    }
+	public int getId() {
+		return personId;
+	}
 
-    public void setId(int id) {
-        this.personId = id;
-    }
+	public void setId(int id) {
+		this.personId = id;
+	}
 
-    public Item getItem() {
-        return item;
-    }
+	public ItemDTO getItem() {
+		return itemDTO;
+	}
 
-    public void setItem(Item item) {
-        this.item = item;
-    }
+	public void setItem(ItemDTO itemDTO) {
+		this.itemDTO = itemDTO;
+	}
 }
